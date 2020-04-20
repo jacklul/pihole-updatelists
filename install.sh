0501996d5969a1d17046bb849f3e7e144a2edf7b
Jack'lul <jacklulcat@gmail.com>
2020-04-20 10:44:15 +0200

    Update install script to detect systemd

--- install.sh
+++ install.sh
@@ -6,11 +6,13 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
 fi
 
 [ -d "/etc/pihole" ] && [ -d "/opt/pihole" ] || { echo "Pi-hole doesn't seem to be installed."; exit 1; }
-command -v curl >/dev/null 2>&1 || { echo "This script requires cURL to run, install it with 'sudo apt install curl'."; exit 1; }
 dpkg -s php-cli >/dev/null 2>&1 || { echo "This script requires PHP-CLI to run, install it with 'sudo apt install php-cli'."; exit 1; }
 
 SPATH=$(dirname $0)
 REMOTE_URL=https://raw.githubusercontent.com/jacklul/pihole-updatelists/beta
+SYSTEMD=`pidof systemd >/dev/null && echo "1" || echo "0"`
+
+[ "$SYSTEMD" == 0 ] && echo "! Systemd not detected, will not install service unit files !"
 
 if [ -f "$SPATH/pihole-updatelists.php" ] && [ -f "$SPATH/pihole-updatelists.conf" ] && [ -f "$SPATH/pihole-updatelists.service" ] && [ -f "$SPATH/pihole-updatelists.timer" ]; then
 	cp -v $SPATH/pihole-updatelists.php /usr/local/sbin/pihole-updatelists && \
@@ -19,9 +21,11 @@ if [ -f "$SPATH/pihole-updatelists.php" ] && [ -f "$SPATH/pihole-updatelists.con
 	if [ ! -f "/etc/pihole-updatelists.conf" ]; then
 		cp -v $SPATH/pihole-updatelists.conf /etc/pihole-updatelists.conf
 	fi
-	
-	cp -v $SPATH/pihole-updatelists.service /etc/systemd/system
-	cp -v $SPATH/pihole-updatelists.timer /etc/systemd/system
+
+	if [ "$SYSTEMD" == 1 ]; then
+    cp -v $SPATH/pihole-updatelists.service /etc/systemd/system
+	  cp -v $SPATH/pihole-updatelists.timer /etc/systemd/system
+	fi
 elif [ "$REMOTE_URL" != "" ]; then
 	wget -nv -O /usr/local/sbin/pihole-updatelists "$REMOTE_URL/pihole-updatelists.php" && \
 	chmod +x /usr/local/sbin/pihole-updatelists
@@ -29,12 +33,16 @@ elif [ "$REMOTE_URL" != "" ]; then
 	if [ ! -f "/etc/pihole-updatelists.conf" ]; then
 		wget -nv -O /etc/pihole-updatelists.conf "$REMOTE_URL/pihole-updatelists.conf"
 	fi
-	
-	wget -nv -O /etc/systemd/system/pihole-updatelists.service "$REMOTE_URL/pihole-updatelists.service"
-	wget -nv -O /etc/systemd/system/pihole-updatelists.timer "$REMOTE_URL/pihole-updatelists.timer"
+
+	if [ "$SYSTEMD" == 1 ]; then
+    wget -nv -O /etc/systemd/system/pihole-updatelists.service "$REMOTE_URL/pihole-updatelists.service"
+	  wget -nv -O /etc/systemd/system/pihole-updatelists.timer "$REMOTE_URL/pihole-updatelists.timer"
+	fi
 else
 	exit 1
 fi
 
-echo "Enabling and starting pihole-updatelists.timer..."
-systemctl enable pihole-updatelists.timer && systemctl start pihole-updatelists.timer
+if [ "$SYSTEMD" == 1 ]; then
+  echo "Enabling and starting pihole-updatelists.timer..."
+  systemctl enable pihole-updatelists.timer && systemctl start pihole-updatelists.timer
+fi
